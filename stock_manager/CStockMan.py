7743db0d15767b28ebb2fc14baafe35acbfc80fb
Michal Horn <michal@apartman.cz>
2019-11-13 15:23:58 +0100

    Move Add stock view to a separate module

--- stock_manager/CStockMan.py
+++ stock_manager/CStockMan.py
@@ -12,6 +12,7 @@ from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 
 from stock_manager.CMainMenuView import MainMenuView
 from stock_manager.CStockView import StockView
+from stock_manager.CAddStockView import AddStockView
 
 from time import sleep
 
@@ -29,47 +30,6 @@ class StockModel(object):
             return {"count": "", "price": "", "shop": ""}
         else:
             return self.get_contact(self.current_id)
-
-
-    
-class AddStockView(Frame):
-    def __init__(self, screen, model):
-        super(AddStockView, self).__init__(screen,
-                                          7,
-                                          60,
-                                          hover_focus=True,
-                                          can_scroll=False,
-                                          title="Zadat sklad",
-                                          reduce_cpu=True)
-        # Save off the model that accesses the contacts database.
-        self._model = model
-
-        # Create the form for displaying the list of contacts.
-        layout = Layout([100], fill_frame=True)
-        self.add_layout(layout)
-        layout.add_widget(Text("Počet kusů:", "count"))
-        layout.add_widget(Text("Nákupní cena:", "price"))
-        layout.add_widget(Text("Místo nákupu:", "shop"))
-        layout.add_widget(Divider())
-        layout2 = Layout([1, 1])
-        self.add_layout(layout2)
-        layout2.add_widget(Button("OK", self._ok), 0)
-        layout2.add_widget(Button("Cancel", self._cancel), 1)
-        self.fix()
-
-    def reset(self):
-        # Do standard reset to clear out form, then populate with new data.
-        super(AddStockView, self).reset()
-        self.data = self._model.get_current_item()
-
-    def _ok(self):
-        self.save()
-        # self._model.update_current_contact(self.data)
-        raise NextScene("StockView")
-
-    @staticmethod
-    def _cancel():
-        raise NextScene("StockView")
 
 class ReduceStockView(Frame):
     def __init__(self, screen, model):
